50d4d35c043e98008f2117008fa23f2ebea19bd5
Konrad Fedorczyk <contact@realhe.ro>
2021-11-01 16:43:55 +0100

    Add ESLint

--- src/scripts/index.ts
+++ src/scripts/index.ts
@@ -3,7 +3,6 @@ import "../styles/app.scss";
 function component() {
   const element = document.createElement("h1");
 
-  // Lodash, currently included via a script, is required for this line to work
   element.innerHTML = "Hello World!";
 
   return element;
